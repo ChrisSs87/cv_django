43e26d7b94fe4e38859d3b82dd0925a87c40a003
ChrisSs10 <chrisss.silva87@gmail.com>
2021-12-02 23:23:41 -0300

    se agrega localhost al webhost

--- Proyecto CV/mysite/settings.py
+++ Proyecto CV/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v5_ei0=-zp3(krjg1=y%5%bp-z%4*lwb$6!k2_x2=fau9#+!*5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', "localhost"]
 
 
 # Application definition
